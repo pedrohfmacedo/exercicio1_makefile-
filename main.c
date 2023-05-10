3125f7fed978a4705a3a1439db8df95ccd4866db
pedrohfmacedo <101015333+pedrohfmacedo@users.noreply.github.com>
2023-05-10 16:15:24 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "calcula.h"
+
+int main() {
+    int num = 5;
+    int resultado = Calcula(num);
+    printf("O quadrado de %d é %d ", num, resultado);
+    printf("\n");
+    return 0;
+}
